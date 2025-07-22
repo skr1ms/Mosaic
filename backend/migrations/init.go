294f78d980da6f206b752db92361da0458d6b1c2
takuya <ilya2007232615@gmail.com>
2025-07-22 18:56:46 +0300

    user logic fix and add nex automigrate structs

--- backend/migrations/init.go
+++ backend/migrations/init.go
@@ -1,6 +1,8 @@
 package migrations
 
 import (
+	"os/user"
+
 	"github.com/skr1ms/mosaic/config"
 	"github.com/skr1ms/mosaic/internal/activation"
 	"github.com/skr1ms/mosaic/internal/coupon"
@@ -10,7 +12,7 @@ import (
 
 func Init(cfg *config.Config) {
 	database := db.NewDB(cfg)
-	err := database.AutoMigrate(&partner.Partner{}, &coupon.Coupon{}, &activation.Activation{})
+	err := database.AutoMigrate(&user.User{}, &partner.Partner{}, &coupon.Coupon{}, &activation.Activation{})
 	if err != nil {
 		panic("Failed to migrate database: " + err.Error())
 	}
