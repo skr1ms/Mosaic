040d64c880e3f094fee9bc931ed37f1ebab8dd8a
takuya <ilya2007232615@gmail.com>
2025-07-22 18:57:21 +0300

    and add new automigrate structs

--- backend/migrations/init.go
+++ backend/migrations/init.go
@@ -7,12 +7,13 @@ import (
 	"github.com/skr1ms/mosaic/internal/activation"
 	"github.com/skr1ms/mosaic/internal/coupon"
 	"github.com/skr1ms/mosaic/internal/partner"
+	"github.com/skr1ms/mosaic/internal/statistics"
 	"github.com/skr1ms/mosaic/pkg/db"
 )
 
 func Init(cfg *config.Config) {
 	database := db.NewDB(cfg)
-	err := database.AutoMigrate(&user.User{}, &partner.Partner{}, &coupon.Coupon{}, &activation.Activation{})
+	err := database.AutoMigrate(&user.User{}, &partner.Partner{}, &coupon.Coupon{}, &activation.Activation{}, &statistics.Statistics{})
 	if err != nil {
 		panic("Failed to migrate database: " + err.Error())
 	}
